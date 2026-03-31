d83fe6d222eecc8f7fac58c33175e940f6cd7d6d
omipatel2311 <omipatel2311@gmail.com>
2026-03-31 07:45:26 +0530

    Add files via upload

---
+++ 3.2.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+int recursiveSum(int arr[], int n)
+{
+    if (n == 0)
+        return 0;
+    return arr[n - 1] + recursiveSum(arr, n - 1);
+}
+
+int iterativeSum(int arr[], int n)
+{
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        sum += arr[i];
+    }
+    return sum;
+}
+
+int main()
+{
+    int n;
+
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter " << n << " elements:\n";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    int recSum = recursiveSum(arr, n);
+    int itrSum = iterativeSum(arr, n);
+
+    cout << "\nRecursive Sum = " << recSum << endl;
+    cout << "Iterative Sum = " << itrSum << endl;
+
+    return 0;
+}
