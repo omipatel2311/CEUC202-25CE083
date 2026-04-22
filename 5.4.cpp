056cf48244b8c8b407c581815bfeb5f60fbf7a14
omipatel2311 <omipatel2311@gmail.com>
2026-04-22 18:01:01 +0530

    Add files via upload

---
+++ 5.4.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Student data
+    string name1 = "John";
+    int marks1 = 85;
+    double fee1 = 1200.50;
+
+    string name2 = "Anna";
+    int marks2 = 90;
+    double fee2 = 1500.75;
+
+    // Display heading
+    cout << "Name   Marks   Fee" << endl;
+
+    // Display data
+    cout << name1 << "   " << marks1 << "   " << fee1 << endl;
+    cout << name2 << "   " << marks2 << "   " << fee2 << endl;
+
+    return 0;
+}
